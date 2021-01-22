59681222618a5d63c2a132f19d6adbf8addcc414
David Sterry <sterry.david@gmail.com>
2021-01-22 09:39:41 -0800

    add routes with versioning to cats controller

--- AnimalShelter/Controllers/CatsController.cs
+++ AnimalShelter/Controllers/CatsController.cs
@@ -2,8 +2,89 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using AnimalShelter.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AnimalShelter.Controller
 {
-    
+    [ApiVersion("1.0")]
+    [Route("api/Cats")]
+    [ApiController]
+    public class CatsV1Controller : ControllerBase
+    {
+        private AnimalShelterContext _db;
+
+        public CatsV1Controller(AnimalShelterContext db)
+        {
+            _db = db;
+        }
+
+        //GET api/cats
+        [HttpGet]
+        public ActionResult <IEnumerable<Cat>> Get()
+        {
+            return _db.Cats.ToList();
+        }
+    }
+
+    [ApiVersion("2.0")]
+    [Route("api/{v:ApiVersion}/Cats")]
+    [ApiController]
+    public class CatsV2Controller : ControllerBase
+    {
+        private AnimalShelterContext _db;
+        
+        public CatsV2Controller(AnimalShelterContext db)
+        {
+            _db = db;
+        }
+
+        //GET api/cats
+        [HttpGet]
+        public ActionResult<IEnumerable<Cat>> Get(string name, int age)
+        {
+            var query = _db.Cats.AsQueryable();
+            if (name !=null)
+            {
+                query = query.Where(entry=>entry.Name == name);
+            }
+            if (age != 0)
+            {
+                query = query.Where(entry => entry.Age = age;)
+            }
+            return query.ToList();
+        }
+
+        //POST api/cats
+        [HttpPost]
+        public void Post([FromBody] Cat cat)
+        {
+            _db.Cats.Add(cat)
+            _db.SaveChanges();
+        }
+
+        //GET api/cats/3
+        [HttpGet("{id}")]
+        public ActionResult<Cat> Get(int id)
+        {
+            return _db.Cats.FirstOrDefault(entry=>entry.CatId == id);
+        }
+
+        //PUT api/cats/3
+        [HttpPUt({"id"})]
+        public void Put(int id, [FromBody] Cat cat)
+        {
+            cat.CatId = id;
+            _db.Entry(cat).State = EntityState.Modefied;
+            _db.SaveChanges();
+        }
+
+        //DELETE api/cats/3
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var catToDelete = _db.Cats.FirstOrDefault(entry=> entry.CatId == id);
+            _db.Cats.Remove(catToDelete);
+            _db.SaveChanges();
+        }
+    }
 }
