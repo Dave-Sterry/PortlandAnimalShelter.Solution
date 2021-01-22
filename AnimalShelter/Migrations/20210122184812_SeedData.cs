212377e7c3cc4deccbefbcac4673c344f7a878d9
David Sterry <sterry.david@gmail.com>
2021-01-22 10:49:12 -0800

    add seeded data to migrations

---
+++ AnimalShelter/Migrations/20210122184812_SeedData.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cats",
+                columns: new[] { "CatId", "Age", "Name" },
+                values: new object[,]
+                {
+                    { 1, 3, "Neptune" },
+                    { 2, 12, "Rosie" },
+                    { 3, 7, "Socks" },
+                    { 4, 8, "Duncan" },
+                    { 5, 4, "Midnight" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Dogs",
+                columns: new[] { "DogId", "Age", "Name" },
+                values: new object[,]
+                {
+                    { 1, 8, "Helo" },
+                    { 2, 11, "Kiva" },
+                    { 3, 12, "Jasper" },
+                    { 4, 3, "Yates" },
+                    { 5, 5, "Nico" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 5);
+        }
+    }
+}
