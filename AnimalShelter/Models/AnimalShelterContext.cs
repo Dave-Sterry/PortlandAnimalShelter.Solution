ead340bd2842992bbdd307d0346db8260b40e0b2
David Sterry <sterry.david@gmail.com>
2021-01-22 10:48:01 -0800

    add data to context.cs file

--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -10,5 +10,26 @@ namespace AnimalShelter.Models
 
         public DbSet<Cat> Cats { get; set; }
         public DbSet<Dog> Dogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Cat>()
+                .HasData(
+                    new Cat { CatId = 1, Name = "Neptune", Age = 3},
+                    new Cat { CatId = 2, Name = "Rosie", Age = 12},
+                    new Cat { CatId = 3, Name = "Socks", Age = 7},
+                    new Cat { CatId = 4, Name = "Duncan", Age = 8},
+                    new Cat { CatId = 5, Name = "Midnight", Age = 4}
+                );
+
+            builder.Entity<Dog>()
+                .HasData(
+                    new Dog { DogId = 1, Name = "Helo", Age = 8},
+                    new Dog { DogId = 2, Name = "Kiva", Age = 11},
+                    new Dog { DogId = 3, Name = "Jasper", Age = 12},
+                    new Dog { DogId = 4, Name = "Yates", Age = 3}, 
+                    new Dog { DogId = 5, Name = "Nico", Age = 5}
+                );
+        }
     }
 }
