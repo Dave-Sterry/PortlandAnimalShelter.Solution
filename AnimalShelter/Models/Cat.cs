c7557b065b79f8dbf99a4a66f0eb342dbd67ef0e
David Sterry <sterry.david@gmail.com>
2021-01-22 08:31:45 -0800

    update models with required statements, errormessages

--- AnimalShelter/Models/Cat.cs
+++ AnimalShelter/Models/Cat.cs
@@ -5,7 +5,11 @@ namespace AnimalShelter.Models
     public class Cat
     {
         public int CatId { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
+        [Required]
+        [Range(1, 30, ErrorMessage = "Age must be between 1-30")
         public int Age { get; set; }
     }
 }
--- AnimalShelter/Models/Dog.cs
+++ AnimalShelter/Models/Dog.cs
@@ -5,7 +5,11 @@ namespace AnimalShelter.Models
     public class Dog
     {
         public int DogId { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
+        [Required]
+        [Range(1, 30, ErrorMessage = "Age must be between 1-30")]
         public int Age { get; set; }
     }
 }
